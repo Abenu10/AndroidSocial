7128d878ca68199c07a3918c7698a0b58c6e73b6
Abenu10 <86177342+Abenu10@users.noreply.github.com>
2022-12-14 10:38:15 -0800

    checking pull request

--- app/src/main/java/org/geeksforgeeks/androidsocial/MainActivity.java
+++ app/src/main/java/org/geeksforgeeks/androidsocial/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     // now write the code on the second branch and commit then push to the master to create  pull request
-
+// do these
     }
 }
